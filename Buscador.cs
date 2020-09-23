c2b61822ec446473f2b664716422f2b7e518934d
Jhonathan Araujo <jhonathanaraujobr@hotmail.com>
2020-09-23 13:08:05 -0300

    Ajustes

--- Buscador.cs
+++ Buscador.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Interface
 {
@@ -11,7 +8,7 @@ namespace Interface
         public IEnumerable<string> GetResultado(string criterio)
         {
             IEnumerable<string> esportes = new List<string> { "Futebol", "Voleibol", "Basquetebol", "Natação", "Rugby", "Handebol" };
-            IEnumerable<string> resultado = new List<string>();
+            IEnumerable<string> resultado;
 
             // Declarativo ou query syntax
             resultado = (from esp in esportes where esp.Contains(criterio) select esp).ToList();
@@ -27,7 +24,7 @@ namespace Interface
         public IEnumerable<string> GetResultado(string criterio)
         {
             IEnumerable<string> times = new List<string> { "Santos", "Palmeiras", "Vasco", "Coritiba", "Bahia" };
-            IEnumerable<string> resultado = new List<string>();
+            IEnumerable<string> resultado;
 
             // Converte - se IEnumerable para List antes de atribuir a resultado
             resultado = times.Where(time => time.ToLower().Contains(criterio.ToLower()))
